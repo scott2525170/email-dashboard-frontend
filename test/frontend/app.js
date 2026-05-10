9251053e1b36b7ea796fd3b5be3c0c478b5cbc2a
thompson ogbe <thompsonogbe52gmail.com>
2026-05-10 17:59:29 +0100

    updated ngrok url

--- test/frontend/app.js
+++ test/frontend/app.js
@@ -9,7 +9,7 @@ document.getElementById("form").addEventListener("submit", async (e) => {
     const file = document.getElementById("image").files[0];
     if (file) formData.append("image", file);
   
-    const API_URL = "https://repaying-mangle-childless.ngrok-free.dev";
+    const API_URL = " https://repaying-mangle-childless.ngrok-free.dev ";
   
     const res = await fetch(`${API_URL}/api`, {
       method: "POST",
